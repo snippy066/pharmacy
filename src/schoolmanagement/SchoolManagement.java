ae93ed47193ff687363a8583984af29f41bcfcdc
Priyanka Thakur <68439548+snippy066@users.noreply.github.com>
2021-04-09 12:39:57 +0530

    first commit

---
+++ src/schoolmanagement/SchoolManagement.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package schoolmanagement;
+
+/**
+ *
+ * @author Snippy066
+ */
+public class SchoolManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      new LoginWindow().setVisible(true);
+    }
+    
+}
